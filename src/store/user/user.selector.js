15bd8a43341835ab5a3da303c0e7567eb9bd5d04
SalmanK81099 <salmanyasinkhan81099@gmail.com>
2022-07-05 12:41:05 +0500

    Selector logic added

--- src/store/user/user.selector.js
+++ src/store/user/user.selector.js
@@ -1,1 +1,38 @@
 export const selectCurrentUser = (state) => state.user.currentUser;
+
+/*
+
+
+
+//Caching logic for selectors with business logic
+//Always save raw data in store and get filtered with selectors:
+
+
+import { createSelector } from 'reselect';
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
+
+export const selectIsLoading = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.isLoading
+);
+
+
+
+
+*/
